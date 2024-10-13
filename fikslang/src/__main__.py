fe69581656b15811e4498f654809834ae99d1524
Petr Stastny <petr.stastny01@gmail.com>
2024-10-13 13:40:06 +0200

    Add state file/instruction list output

--- fikslang/src/__main__.py
+++ fikslang/src/__main__.py
@@ -1,20 +1,34 @@
 import json
 import sys
+from contextlib import suppress
 
 from fikslang.src.machine import Machine
 
 
 def main() -> None:
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: fikslang <source>")
         return
 
     with open(sys.argv[1], "r") as f:
         source = f.read()
 
+    if len(sys.argv) == 3:
+        # We probably received state file as well
+        with open(sys.argv[2], "r") as f:
+            state = json.load(f)
+    else:
+        state = None
+
     try:
         machine = Machine(source)
-        machine.execute()
+
+        if state is not None:
+            machine.memory.stack = state["stack"]
+            machine.memory.heap = state["memory"]
+
+        with suppress(SystemExit):
+            machine.execute()
 
         if sys.stdout.isatty():
             print("Stack:")
@@ -27,6 +41,10 @@ def main() -> None:
                     {
                         "stack": machine.memory.stack,
                         "memory": machine.memory.heap,
+                        "instructions": [
+                            instruction.opcode
+                            for instruction in machine.execution_memory
+                        ],
                     }
                 )
             )
